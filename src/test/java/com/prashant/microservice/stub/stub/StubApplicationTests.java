0466b03fa7d6728704f0fcec11498329fd893a16
Prashant Kumar Singh <prashant.sirt153@gmail.com>
2019-07-15 16:32:01 +0530

    Microservices-stub for java based services

---
+++ src/test/java/com/prashant/microservice/stub/stub/StubApplicationTests.java
@@ -0,0 +1,16 @@
+package com.prashant.microservice.stub.stub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
